e9bac9162009f15277c7c6d73c51a5dd0e4af351
New Horizons Computer Learning Centers of Singapore <training@newhorizons.com.sg>
2019-02-08 19:47:01 +0800

    initial release

--- weather app/ViewController.swift
+++ weather app/ViewController.swift
@@ -10,14 +10,106 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet var cityTextField: UITextField!
+    
+    @IBOutlet var resultLabel: UILabel!
+    
+
+    @IBAction func showWeather(sender: AnyObject) {
+        
+        var wasSuccessful = false
+        let attemptedUrl = NSURL(string: "https://www.weather-forecast.com/locations/" + cityTextField.text!.stringByReplacingOccurrencesOfString(" ", withString: "-") + "/forecasts/latest")
+        
+        if let url = attemptedUrl {
+        
+        let task = NSURLSession.sharedSession().dataTaskWithURL(url) { (data, response, error) -> Void in
+            
+            if let urlContent = data {
+                
+                let webContent = NSString(data: urlContent, encoding: NSUTF8StringEncoding)
+                
+                //let websiteArray = webContent!.componentsSeparatedByString("3 Day Weather Forecast Summary:</b><span class=\"read-more-small\"><span class=\"read-more-content\"> <span class=\"phrase\">")
+                
+
+                let websiteArray  = webContent!.componentsSeparatedByString("Live weather stations")
+                
+                if websiteArray.count > 1 {
+                    
+                    //print(websiteArray.count)
+
+
+                    let weatherArray = websiteArray[1].componentsSeparatedByString("</span>")
+                    
+                    
+                    if weatherArray.count > 1 {
+                        
+                        //wasSuccessful = true
+                        
+                        //print(weatherArray.count)
+                        print(weatherArray[6])
+                        var tempArray = weatherArray[6].componentsSeparatedByString("<span class=\"temp\">")
+                        
+                        if tempArray.count > 1 {
+                            
+                            wasSuccessful = true
+                            
+                        //let weatherSummary = weatherArray[0].stringByReplacingOccurrencesOfString("&deg;", withString: "º")
+                            
+                            var weatherSummary = tempArray[1].stringByAppendingString("ºC")
+                        
+                        print(tempArray[1])
+                        
+                        dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                            
+                            self.resultLabel.text = weatherSummary
+                            
+                            
+                        })
+                        
+                        
+                        
+                    }
+                    
+                    }
+                    
+                    
+                }
+                
+                
+                
+            }
+            
+            if wasSuccessful == false {
+                
+                self.resultLabel.text = "Couldn't find the weather for that city - please try again."
+                
+                
+            }
+            
+        }
+        
+        task.resume()
+        
+        } else {
+            
+            self.resultLabel.text = "Couldn't find the weather for that city - please try again."
+            
+            
+        }
+
+    }
+	
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+        
+    
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        // Dispose of anyresources that can be recreated.
     }
 
 
